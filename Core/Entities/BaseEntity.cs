83195e270b9f3c68502ed74c3d79d3a893ed35a6
Antonio Lopes Da Cruz <tonycruz@outlook.com>
2020-04-20 21:41:42 +0100

    End of Section 13

--- Core/Entities/BaseEntity.cs
+++ Core/Entities/BaseEntity.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Core.Entities
+﻿namespace Core.Entities
 {
-   public class BaseEntity
+    public class BaseEntity
     {
         public int Id { get; set; }
     }
