f82d26227a596ca261ad740925bcf85de7c0741f
Antonio Lopes Da Cruz <tonycruz@outlook.com>
2020-04-21 19:47:40 +0100

    End of  Section 15

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,7 @@ using API.Helpers;
 using API.Middleware;
 using AutoMapper;
 using Infrastructure.Data;
+using Infrastructure.Data.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -30,12 +31,17 @@ namespace API
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<AppIdentityDbContext>(x =>
+            {
+                x.UseSqlite(_config.GetConnectionString("IdentityConnection"));
+            });
             services.AddSingleton<IConnectionMultiplexer>(c =>
            {
                var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
                return ConnectionMultiplexer.Connect(configuration);
            });
             services.AddApplicationServices();
+            services.AddIdentityServices(_config);
             services.AddSwaggerDocumentation();
             services.AddCors(opt =>
             {
@@ -58,6 +64,7 @@ namespace API
             app.UseStaticFiles();
             app.UseCors("CorsPolicy");
 
+            app.UseAuthentication();
             app.UseAuthorization();
             app.UseSwaggerDocumention();
             app.UseEndpoints(endpoints =>
---
+++ Infrastructure/Data/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUsersAsync(UserManager<AppUser> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var user = new AppUser
+                {
+                    DisplayName = "Tony Cruz",
+                    Email = "tony@test.com",
+                    UserName = "tony@test.com",
+                    Address = new Address
+                    {
+                        FirstName = "Tony",
+                        LastName = "Cruz",
+                        Street = "8000 High Road",
+                        City = "London",
+                        State = "Leyton",
+                        Zipcode = "E15 2DD"
+                    }
+                };
+
+                await userManager.CreateAsync(user, "Tony@123456");
+            }
+        }
+    }
+}
